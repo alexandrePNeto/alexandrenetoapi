0ae58b397ec902ed31e7464fc201eafe8918a32b
Nethobr <leezando.neto@gmail.com>
2025-10-07 21:22:20 -0300

    first commit

---
+++ src/main/java/br/edu/infnet/alexandrenetoapi/AlexandrenetoapiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.alexandrenetoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexandrenetoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlexandrenetoapiApplication.class, args);
+	}
+
+}

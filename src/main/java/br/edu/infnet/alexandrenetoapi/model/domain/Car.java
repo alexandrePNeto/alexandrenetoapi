c6e3a89a6b6c99872f13ac3a1affd01426e206f1
Nethobr <leezando.neto@gmail.com>
2025-10-13 20:34:13 -0300

    feat: implementada a Feature 1

--- src/main/java/br/edu/infnet/alexandrenetoapi/model/domain/Car.java
+++ src/main/java/br/edu/infnet/alexandrenetoapi/model/domain/Car.java
@@ -1,22 +1,60 @@
 package br.edu.infnet.alexandrenetoapi.model.domain;
 
 public class Car {
+    private Integer id;
     private String name;
+    private String color;
+    private String plate;
+    private String size;
 
-    public Car(String name) {
-        this.name = name;
+    public Integer getId() {
+        return id;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getSize() {
+        return size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getPlate() {
+        return plate;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
     @Override
     public String toString() {
-        return "name=" + this.name;
+        return ""
+            + this.id.toString() + ","
+            + "name=" + this.name + ","
+            + "color=" + this.color + ","
+            + "plate=" + this.plate + ","
+            + "size=" + this.size
+        ;
     }
 }
